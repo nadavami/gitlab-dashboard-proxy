e5dcfea6e1df048bc05718aee0fed21a9fc910a6
Nadav Ami <nadav.ami@bell.ca>
2018-07-23 13:58:19 -0400

    Forward requests from /gitlab/ to GITLAB_URL endpoint

--- app/app.js
+++ app/app.js
@@ -1,10 +1,13 @@
 const express = require('express')
+const proxy = require('http-proxy-middleware')
 
 let app = express()
 
-app.use('/gitlab', (req, res) => {
-  res.status(200)
-  res.end()
-})
+app.use('/gitlab', proxy({
+  target: process.env.GITLAB_URL,
+  changeOrigin: true,
+  pathRewrite: { '^/gitlab': '/' },
+  secure: false
+}))
 
 module.exports = app
--- tests/app.test.js
+++ tests/app.test.js
@@ -1,10 +1,20 @@
 /* eslint-env jest */
 const request = require('supertest')
+const nock = require('nock')
+
+// I don't like this.
+// This sets the configuration once in app.js. It seems like it could introduce side-effects.
+const dummyGitLabEndpoint = 'http://some-gitlab-instance.com'
+process.env.GITLAB_URL = dummyGitLabEndpoint
+
 const app = require('../app/app')
 
 describe('Forwards GitLab API Requests', () => {
-  test('Can capture all requests on /gitlab/', async () => {
+  test('Can forward /gitlab/ requests to GitLab', async () => {
+    nock(dummyGitLabEndpoint).get('/api/v4/projects/').reply(200, {msg: 'some content'})
+
     let response = await request(app).get('/gitlab/api/v4/projects/')
     expect(response.statusCode).toBe(200)
+    expect(response.body).toEqual({msg: 'some content'})
   })
 })
