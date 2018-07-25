d249a2971d06a036a4890cd995abc4343daa5302
Nadav Ami <nadav.ami@bell.ca>
2018-07-25 13:40:40 -0400

    Cache requests after proxying for the first time
    Request bodies and headers are cached in the paths object after they
    first pass through the proxy.

--- app/app.js
+++ app/app.js
@@ -4,12 +4,29 @@ const proxy = require('express-http-proxy')
 const GITLAB_URL = process.env.GITLAB_URL
 
 let app = express()
+let paths = {}
 
-app.use('/gitlab', proxy(GITLAB_URL, {
-  proxyReqOptDecorator: reqOptions => {
-    reqOptions.rejectUnauthorized = false
-    return reqOptions
+app.use('/gitlab', (req, res, next) => {
+  let path = req.path.replace(/gitlab\//, '')
+  if (path in paths) {
+    res.set(paths[path].headers)
+    res.end(paths[path].body)
+  } else {
+    proxy(GITLAB_URL, {
+      userResHeaderDecorator: (headers, userReq, userRes, proxyReq, proxyRes) => {
+        paths[userReq.path] = {}
+        paths[userReq.path].headers = headers
+        return headers
+      },
+      userResDecorator: (proxyRes, proxyResData, userReq, userRes) => {
+        paths[userReq.path].body = proxyResData
+        return proxyResData
+      },
+      proxyReqOptDecorator: reqOptions => {
+        reqOptions.rejectUnauthorized = false
+        return reqOptions
+      }
+    })(req, res, next)
   }
-}))
-
+})
 module.exports = app
--- tests/app.test.js
+++ tests/app.test.js
@@ -19,11 +19,26 @@ afterEach(() => {
 
 describe('Forwards GitLab API Requests', () => {
   test('Can forward /gitlab/ requests to GitLab', async () => {
-    let gitlab = nock(dummyGitLabEndpoint).get('/api/v4/projects/').reply(200, {msg: 'some content'})
+    let gitlab = nock(dummyGitLabEndpoint)
+      .get('/api/v4/projects/')
+      .reply(200, {msg: 'some content'})
 
     let response = await request(app).get('/gitlab/api/v4/projects/')
     expect(response.statusCode).toBe(200)
     expect(response.body).toEqual({msg: 'some content'})
     expect(gitlab.isDone()).toBe(true)
+  })
+
+  test('Can cache requests to GitLab', async () => {
+    let gitlab = nock(dummyGitLabEndpoint)
+      .get('/api/v4/projects/')
+      .times(2)
+      .reply(200, {msg: 'some content'})
+
+    await request(app).get('/gitlab/api/v4/projects/')
+    let response = await request(app).get('/gitlab/api/v4/projects/')
+    expect(response.statusCode).toBe(200)
+    expect(response.body).toEqual({msg: 'some content'})
+    expect(gitlab.isDone()).toBe(false)
   })
 })
