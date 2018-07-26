ada92d3b880069eb490b8a32a27b8eaf62b8c87c
Nadav Ami <nadav.ami@bell.ca>
2018-07-26 09:16:51 -0400

    Check if GITLAB_URL is set before doing anything

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 
-const app = require('./proxy')
-let port = process.env.PORT || 3000
-
 if (!process.env.GITLAB_URL) {
   console.error('Error! GitLab URL is not set.\n')
   console.log('Usage:\n export GITLAB_URL=https://gitlab.com\n gitlab-dashboard-proxy')
   process.exit(1)
 }
+
+const app = require('./proxy')
+let port = process.env.PORT || 3000
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`)
